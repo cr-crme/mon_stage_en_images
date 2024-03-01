ddce3dfedcbf86f43c72111959a4aed38bb6fd94
Pariterre <pariterre@hotmail.com>
2024-03-01 18:03:40 -0500

    Fixed modifying student

--- lib/screens/all_students/widgets/new_student_alert_dialog.dart
+++ lib/screens/all_students/widgets/new_student_alert_dialog.dart
@@ -48,6 +48,7 @@ class _NewStudentAlertDialogState extends State<NewStudentAlertDialog> {
       userType: UserType.student,
       mustChangePassword: true,
       companyNames: [_companyName ?? ''],
+      id: widget.student?.id,
     );
 
     Navigator.pop(context, student);
